0dda11c3d97ae5be461fdd02db1a889cc4d74ac3
Raphael <raphael_riviere@yahoo.fr>
2017-04-18 16:05:16 +0400

    Ajoute de la suppression et de la modification d'url

--- assets/js/default/directory.js
+++ assets/js/default/directory.js
@@ -1171,25 +1171,36 @@ var directory = {
     // ********************************
     // URL DIRECTORY PANEL
     // ********************************
-    urlPanelHtml : function(params){
-        mylog.log("-----------urlPanelHtml", params);
-        str = "";  
-        str += "<div class='col-lg-4 col-md-6 col-sm-6 col-xs-12 margin-bottom-10 searchEntityContainer'>";
-          str += '<a href="'+params.url+'" target="_blank" class="text-dark tooltips"'+
-                    'data-toggle="tooltip" data-placement="top" data-original-title="'+params.url+'" >';
-            str += "<div class='searchEntity'>";
-              
-                str += "<div class='panel-heading border-light col-lg-12 col-md-12 col-sm-12 col-xs-12'>";
-                  str += '<h4 class="panel-title text-dark pull-left">'+params.title+'</h4>';
-                //str += "</div>";
-                //str += "<div class='panel-body no-padding'>";
-                    str += '<br/><span class="" style="font-size: 11px !important;">'+params.type+'</span>';
-                str += "</div>";
-            
-            str += "</div>";
-          str += '</a>';
-        str += "</div>";
-        return str;
+    urlPanelHtml : function(params, key){
+		mylog.log("-----------urlPanelHtml", params);
+		str = "";  
+		str += "<div class='col-lg-4 col-md-6 col-sm-6 col-xs-12 margin-bottom-10'>";
+			str += "<div class='searchEntity'>";
+				str += '<ul class="nav navbar-nav btn-params-directory">';
+					str += '<li class="text-left">';
+						str += '<a href="javascript:;" onclick="updateUrl(\''+key+'\', \''+params.title+'\',  \''+params.url+'\', \''+params.type+'\');" ' +
+								'class="bg-white tooltips" data-toggle="tooltip" data-placement="top" data-original-title="'+trad["update"]+'" >';
+							str += '<i class="fa fa-pencil"></i>';
+						str += '</a>';
+					str += '</li>';
+					str += '<li class="text-left">';
+						str += '<a href="javascript:;" onclick="removeUrl(\''+key+'\');" class="bg-white tooltips" '+
+								'data-toggle="tooltip" data-placement="top" data-original-title="'+trad["delete"]+'" >';
+							str += '<i class="fa fa-trash"></i>';
+						str += '</a>';
+					str += '</li>';
+				str += '</ul>';
+
+				str += '<a href="'+params.url+'" target="_blank" class="text-dark tooltips col-xs-8"'+
+						'data-toggle="tooltip" data-placement="top" data-original-title="'+params.url+'" >';
+					str += "<div class='panel-heading border-light col-lg-12 col-md-12 col-sm-12 col-xs-12'>";
+						str += '<h4 class="panel-title text-dark pull-left">'+params.title+'</h4>';
+						str += '<br/><span class="" style="font-size: 11px !important;">'+params.type+'</span>';
+					str += "</div>";
+				str += '</a>';
+			str += "</div>";
+		str += "</div>";
+		return str;
     },
     // ********************************
     // ROOMS DIRECTORY PANEL
@@ -1460,7 +1471,7 @@ var directory = {
 
           }else{
             if(contentType == "urls")
-                  str += directory.urlPanelHtml(params);
+                  str += directory.urlPanelHtml(params, i);
           }
         }); //end each
         mylog.log("END -----------showResultsDirectoryHtml ("+str.length+")")

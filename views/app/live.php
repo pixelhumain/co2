8fa99cb2f5c5e0bf8bc299bacb53979d7f4b4947
Bouboule <clement.damiens@gmail.com>
2017-05-02 05:52:01 +0200

    different name startsearch for directory and live

--- views/app/live.php
+++ views/app/live.php
@@ -121,13 +121,13 @@ jQuery(document).ready(function() {
       slidupScopetagsMin();
     });
 	$('#btn-start-search').click(function(e){
-		startSearch(false);
+		startNewsSearch(false);
     });
 		
 	
     
     searchPage = true;
-	startSearch(true);
+	startNewsSearch(true);
 
 	$(".titleNowEvents .btnhidden").hide();
 
@@ -181,7 +181,7 @@ function initFreedomInterface(){
 }
 
 var timeout;
-function startSearch(isFirst){
+function startNewsSearch(isFirst){
 	//Modif SBAR
 	//$(".my-main-container").off();
 	//if(liveScopeType == "global"){

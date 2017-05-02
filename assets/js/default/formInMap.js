7b93d5dbd9de5df750ae2e2cfd091031a1b1ac3c
Raphael <raphael_riviere@yahoo.fr>
2017-05-02 16:51:23 +0400

    modif FormInMap

--- assets/js/default/formInMap.js
+++ assets/js/default/formInMap.js
@@ -152,7 +152,7 @@ var formInMap = {
 		// ---------------- newElement_city
 		$('[name="newElement_city"]').keyup(function(){ 
 			$("#dropdown-city-found").show();
-			if($('[name="newElement_city"]').val().length > 0){
+			if($('[name="newElement_city"]').val().length > 1){
 				formInMap.NE_city = $('[name="newElement_city"]').val();
 				formInMap.changeSelectCountrytim();
 
@@ -719,9 +719,9 @@ var formInMap = {
 	changeSelectCountrytim : function(){
 		mylog.log("changeSelectCountrytim", formInMap.NE_country);
 		mylog.log("formInMap.NE_cp.substring(0, 3)");
-		countryFR = ["FR","GP","MQ","GF","RE","PM","YT"];
-
-		if(countryFR.indexOf(formInMap.NE_country) != -1){
+		var countryFR = ["FR","GP","MQ","GF","RE","PM","YT"];
+		var regexNumber = new RegExp("[1-9]+") ;
+		if(countryFR.indexOf(formInMap.NE_country) != -1 && regexNumber.test(formInMap.NE_country) ) {
 			var name = $('[name="newElement_city"]').val();
 			if(name.substring(0, 3) == "971")
 				$('[name="newElement_country"]').val("GP");

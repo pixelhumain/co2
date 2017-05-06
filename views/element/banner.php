ef6ab905e852194261f3f1383abd201a959838f2
Bouboule <clement.damiens@gmail.com>
2017-05-06 18:41:17 +0200

    Lien parent => "Programmé pendant"

--- views/element/banner.php
+++ views/element/banner.php
@@ -163,7 +163,7 @@
 			<div class="section-date pull-right">
 			<div style="font-size: 14px;font-weight: none;">
 			<?php if(@$element['parent']){ ?>
-			<?php echo Yii::t("common","Created by") ?> : <a href="#page.type.<?php  echo $element['parentType']; ?>.id.<?php  echo $element['parentId']; ?>" class="lbh"> <i class="fa fa-calendar"></i> <?php  echo $element['parent']['name']; ?></a><br/>
+			<?php echo Yii::t("common","Planned on") ?> : <a href="#page.type.<?php  echo $element['parentType']; ?>.id.<?php  echo $element['parentId']; ?>" class="lbh"> <i class="fa fa-calendar"></i> <?php  echo $element['parent']['name']; ?></a><br/>
 			<?php } ?>
 			<?php echo Yii::t("common","Organized by") ?> : <a href="#page.type.<?php  echo $element['organizerType']; ?>.id.<?php  echo $element['organizerId']; ?>" class="lbh"> <i class="fa text-<?php  echo Element::getColorIcon($element['organizerType']); ?> fa-<?php  echo Element::getFaIcon($element['organizerType']); ?>"></i> <?php  echo $element['organizer']['name']; ?></a>
 		</div>

8766d3f1d57d643f50798757fdfd77a098dc202b
Oceatoon <oceatoon@gmail.com>
2017-07-18 16:16:58 +0400

    added text to funding doc

--- views/docs/panels/kickerFunding.php
+++ views/docs/panels/kickerFunding.php
@@ -1,4 +1,4 @@
-<div class="panel-heading border-light center text-dark partition-white radius-10 " style="margin-bottom:50px;">
+<div class="panel-heading border-light center text-dark partition-white radius-10 " style="margin-bottom:30px;">
 	<img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/CO2r.png" style="margin-bottom:20px;" class="nc_map" height=50><br/>
 	<span class="panel-title homestead text-red " style="font-size: 48px">Aidez la vie d'un communs</span> </span>
 </div>
@@ -30,12 +30,62 @@
 		  </h2>
 		  
 		  <h3 class="text-dark"><?php echo Yii::t("loader","Help us to forward our work") ?>...</h3>
-		  <a href="#info.p.finance" target="_blank" class="btnHelp btn btn-link">
-		    <i class="fa fa-hand-o-right"></i> <?php echo Yii::t("loader","More informations about our financial situation") ?>
-		  </a>
-		  <br/>
 		  <a href="https://www.helloasso.com/associations/open-atlas/adhesions/soutenez-et-adherez-a-open-atlas" target="_blank" class="btnHelp btn btn-warning margin-top-10 text-dark">
 		        <i class="fa fa-gift"></i> <?php echo Yii::t("loader","Make a donation to the NGO") ?>
 		  </a> 
 	</div>
+</div>
+
+
+<div class="row" id="sitFin">
+    <div class="row main-apropos padding-top-15 padding-bottom-50">
+	    
+        <div class="col-lg-3 col-md-3 col-sm-4 text-right hidden-xs" id="sub-menu-left">
+        </div>
+        <div class="col-lg-7 col-md-8 col-sm-7 col-xs-12">
+
+        	<h5 class="pull-left">
+        		<i class="fa fa-angle-down"></i> Situation financière
+        	</h5>
+        	
+        	<br>
+        	<hr>
+
+        	<h1 class="text-red font-blackoutM" id="koica">
+        		On reste fragile !!!
+        	</h1>
+
+        	<h3 class="letter-blue"><i class="fa fa-angle-right"></i> Please help us !</h3>
+        	<p>
+				Le collectif de développeurs qui s'occupe actuellement de la plateforme a besoin de pouvoir continuer à travailler à plein temps sur ce projet pharaonique.
+				<br><br>
+				Impossible pour eux de travailler à mi-temps, et de partager leur travail avec d'autres prestations.<br>
+				La maintenance, l'amélioration, et le suivi de l'application nécessite une équipe de techniciens compétent qui s'en occupe quotidiennement.
+				<br><br>
+				Pour que nos développeurs puissent se consacrer entièrement au projet Communecter, ils ont besoin de votre soutient financier.
+				<br><br>
+				Le compte de l'association sont aujourd'hui au plus bas, ce qui laisse entrevoir la possibilité de l'abandon de la plateforme, faute de moyen financier suffisant pour permettre aux développeurs de vivre sans travailler "à côté".
+				<br><br>
+				Nous nous en remettons donc à vos dons.
+				<br><br>
+				Si vous souhaitez que le réseau Communecter continue à grandir, il ne tient qu'à vous d'apporter votre soutien. Si chacun d'entre vous donne quelques euros, c'est possible !
+
+				<h3 class="letter-blue">Un projet à long terme</h3>
+				L'équipe actuelle de développeurs est sur-motivée et souhaite plus que tout continuer son travail.<br>
+				Elle souhaite également s'agrandir si possible, en intégrant de nouveau développeurs, ou en collaborant avec d'autres équipes afin de développer des applications interropérables.<br><br>
+				Nous souhaitons également continuer à améliorer l'application Communecter, en rajoutant de nouvelles fonctionnalités.
+
+				<h3 class="letter-blue">Budget annuel : (exemple)</h3>
+				6 développeurs à 2000€/mois, pendant 1 an : Pour développer l'application<br>
+				6 * 2000 * 12 = <?php $c1 = 6 * 2000 * 12; echo $c1; ?>€
+				<br><br>
+				6 communecteur à 1000€/mois, pendant 1 an : Pour faire connaitre et comprendre l'application<br>
+				6 * 2000 * 12 = <?php $c2 = 6 * 1000 * 12; echo $c2; ?>€
+				<br><br>
+				ETC ...
+				<br><br><br>
+				Total : <?php echo $c1 + $c2; ?>€
+        	</p>
+        </div>
+    </div>
 </div>

fa423aed3c98b60cb717f88a5dcd530f4ef8d742
Oceatoon <oceatoon@gmail.com>
2017-07-18 16:03:24 +0400

    adding contribution au menu

---
+++ views/docs/panels/kickerFunding.php
@@ -0,0 +1,41 @@
+<div class="panel-heading border-light center text-dark partition-white radius-10 " style="margin-bottom:50px;">
+	<img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/CO2r.png" style="margin-bottom:20px;" class="nc_map" height=50><br/>
+	<span class="panel-title homestead text-red " style="font-size: 48px">Aidez la vie d'un communs</span> </span>
+</div>
+
+<style type="text/css">
+	.keypane{
+		height: 235px;
+	}
+</style>
+
+<div class="row">
+	<div class="col-md-6 col-md-offset-3">
+		<div class="col-xs-6 center">
+		    <a href="https://www.helloasso.com/associations/open-atlas/adhesions/soutenez-et-adherez-a-open-atlas/2?sessionId=dvutjo5jusdgs1cvmmkui4su&widget=False" target="_blank" class="btn btn-default text-red bold">
+		        <i class="fa fa-heart fa-2x"></i> <span class="text-dark"> Membre ( 1€/mois ) </span>
+		     </a>     
+		</div>
+		<div class="col-xs-6 center">
+		     <a href="https://www.helloasso.com/associations/open-atlas/adhesions/soutenez-et-adherez-a-open-atlas/2?sessionId=dvutjo5jusdgs1cvmmkui4su&widget=False" target="_blank" class="btn btn-default text-red bold ">
+		        <i class="fa fa-heart fa-2x"></i> <span class="text-dark"> Sponsor ( 50€/mois ) </span> <i class="fa fa-heart fa-2x"></i>
+		     </a>      
+		</div>
+	</div>
+	<div class="alert col-md-6 col-md-offset-3 shadow2  margin-top-10" style="">
+		  <i class="fa fa-puzzle-piece faa-pulse animated fa-5x padding-10"></i> <i class="fa fa-plus fa-2x padding-10"></i> <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/piggybank.png" height=70> <i class="fa fa-plus fa-2x padding-10"></i> <i class="fa fa-group faa-pulse animated fa-5x padding-10"></i> <span class="bold" style="font-size:40px;">=</span> <i class="fa fa-heartbeat faa-pulse text-red animated fa-5x padding-10"></i>
+		  <br/><br/><h2 style="font-weight:300; margin:-35px 0 0 0;" class="homestead padding-10">
+		    <span class="text-red"><?php echo Yii::t("loader","1 little coin for a great puzzle") ?></span>
+		    <br/><a href="https://www.helloasso.com/associations/open-atlas/adhesions/soutenez-et-adherez-a-open-atlas" target="_blank" class="text-dark"><?php echo Yii::t("loader","Contribute") ?> <i class="fa  fa-arrow-circle-right"></i></a>
+		  </h2>
+		  
+		  <h3 class="text-dark"><?php echo Yii::t("loader","Help us to forward our work") ?>...</h3>
+		  <a href="#info.p.finance" target="_blank" class="btnHelp btn btn-link">
+		    <i class="fa fa-hand-o-right"></i> <?php echo Yii::t("loader","More informations about our financial situation") ?>
+		  </a>
+		  <br/>
+		  <a href="https://www.helloasso.com/associations/open-atlas/adhesions/soutenez-et-adherez-a-open-atlas" target="_blank" class="btnHelp btn btn-warning margin-top-10 text-dark">
+		        <i class="fa fa-gift"></i> <?php echo Yii::t("loader","Make a donation to the NGO") ?>
+		  </a> 
+	</div>
+</div>

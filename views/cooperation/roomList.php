a1d26bfe903b76ed13ae1a83d0354f787367b800
Kgneo <tristan.goguet@gmail.com>
2017-09-05 19:52:46 +1100

    dda fix

--- views/cooperation/roomList.php
+++ views/cooperation/roomList.php
@@ -1,19 +1,17 @@
 
-<?php if(!@$auth)
+<?php if(!isset($auth))
 		$auth = Authorisation::canParticipate(Yii::app()->session['userId'], @$post["parentType"], @$post["parentId"]);
 ?>
-
+<?php if($auth){ ?>
+	
 <li class="submenucoop sub-rooms">
-	<?php if($auth){ ?>
-		<a href="javascript:dyFObj.openForm('room')" class="btn btn-link letter-green bold">
-	  		<i class="fa fa-plus-circle"></i> <?php echo Yii::t("cooperation", "Create room") ?>
-	  	</a>
-	<?php } ?>
-	<!-- <a href="javascript:" class="text-dark padding-left-15" id="btn-update-coop">
-  		<i class="fa fa-refresh"></i> <?php echo Yii::t("cooperation", "Refresh data") ?>
-  	</a> -->
+	<a href="javascript:dyFObj.openForm('room')" class="btn btn-link letter-green bold">
+  		<i class="fa fa-plus-circle"></i> <?php echo Yii::t("cooperation", "Create room") ?>
+  	</a>
 </li>
 <li class="submenucoop sub-rooms"><hr></li>
+<?php } ?>
+
 
 <?php foreach($roomList as $key => $room){ ?>
 	<li class="submenucoop sub-rooms">

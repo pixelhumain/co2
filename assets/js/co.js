0b7fc2b8c74b5536b8cfc7b26ebd54a2eb4d1510
Bouboule <clement.damiens@gmail.com>
2017-09-07 16:45:44 +0200

    linkify news text

--- assets/js/co.js
+++ assets/js/co.js
@@ -158,6 +158,23 @@ function bindRightClicks() {
 	    }
 	});
 }
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
 function addslashes(str) {
 	  //  discuss at: http://phpjs.org/functions/addslashes/
 	  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)

d085febd2f11552a576d93eb60030103e588530d
codev <raphaelriviere06@gmail.com>
2017-09-18 15:03:24 +0400

    Modif

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -1339,4 +1339,15 @@ La vie en santé;Santé;;
 		echo  "NB Cities : " .$nbcities."<br>" ;
 		echo $str;
 	}
+
+
+	public function actionPdf(){
+		require __DIR__.'/vendor/autoload.php';
+
+		use Spipu\Html2Pdf\Html2Pdf;
+
+		$html2pdf = new Html2Pdf();
+		$html2pdf->writeHTML('<h1>HelloWorld</h1>This is my first test');
+		$html2pdf->output();
+	}
 }

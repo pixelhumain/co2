da293d3895c8c8505edc7201d73b2f7b117be0aa
codev <raphaelriviere06@gmail.com>
2017-11-06 17:28:20 +0300

    Modif

--- views/pod/whycommunexion.php
+++ views/pod/whycommunexion.php
@@ -5,17 +5,16 @@
 	<center class="caretExplain blue"><i class="fa fa-caret-down"></i><br/></center>
 	<div class="explainDesc">
 		Ce n'est pas la technologie qui changera la société mais une population bien outillée.
-		<br/>Se communecter c'est etre citoyen acteur locale
+		<br/>Se communecter c'est être un citoyen, un acteur local
 		<br/>Me communecter me permet :  
 		<ul>
 			<li><i class="fa fa-angle-right"></i> d'agir localement et pas juste m'informer</li>
 			<li><i class="fa fa-angle-right"></i> de voir les acteurs autour de moi</li>
-			<li><i class="fa fa-angle-right"></i> de référencer et valoriser les choses auxquelles je crois ici et là</li>
+			<li><i class="fa fa-angle-right"></i> de référencer et valoriser les choses auxquelles je crois</li>
 			
 			<li><i class="fa fa-angle-right"></i> je participe à l'activité citoyenne locale</li>
 			<li><i class="fa fa-angle-right"></i> je partage pour améliorer mon territoire</li>
-			<li><i class="fa fa-angle-right"></i> je fais des propositions aux autres habitants (Discuter, Décider , Agir)</li>
+			<li><i class="fa fa-angle-right"></i> je fais des propositions aux autres habitants (Discuter, Décider ,Agir)</li>
 		</ul>
 	</div>
 </div>
-

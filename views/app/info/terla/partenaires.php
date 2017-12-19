799dda27d0c4d244c35020f5b14419db5a558f62
Kgneo <tristan.goguet@gmail.com>
2017-12-19 15:05:30 +1100

    init link info footer

---
+++ views/app/info/terla/partenaires.php
@@ -0,0 +1,17 @@
+<?php 
+    //header + menu
+    $layoutPath = 'webroot.themes.'.Yii::app()->theme->name.'.views.layouts.';
+    $this->renderPartial('../app/info/terla/infocommon', 
+            array( "title"=> "Nos partenaires", "layoutPath" => $layoutPath ));
+?>
+
+<style type="text/css">
+</style>
+
+<section class="padding-top-70">
+    <div class="container main-apropos padding-top-15 padding-bottom-50">
+	    Hello partenaires
+    </div>
+</section>
+
+<?php $this->renderPartial($layoutPath.'footer',  array( "subdomain"=>"info" ) ); ?>

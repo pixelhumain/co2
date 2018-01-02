e444f203791e50d6152b43183cab1d72e6812ca7
Bouboule <clement.damiens@gmail.com>
2018-01-02 16:00:38 +0100

    search page

--- views/app/page.php
+++ views/app/page.php
@@ -131,7 +131,7 @@ jQuery(document).ready(function() {
     
 	//initKInterface({"affixTop":0});
 	$("#mainNav").addClass("affix");
-	//initPageInterface();
+	initPageInterface();
     // var tpl = '<?php //echo @$_GET["tpl"] ? $_GET["tpl"] : "profilSocial"; ?>';
 	// getAjax('#onepage' ,baseUrl+'/'+moduleId+"/element/detail/type/"+type+"/id/"+id+"/view/"+view+"?tpl="+tpl,function(){ 
 	// 	initPageInterface();

faee14590ba08cb4ec97dc66ebf16bab11f77966
Bouboule <clement.damiens@gmail.com>
2018-02-21 16:44:36 +0100

    trad dossier & welcome

---
+++ views/docs/de/commandement.php
@@ -0,0 +1,97 @@
+<div class="panel-heading border-light center text-dark partition-white radius-10">
+	<span class="panel-title homestead"> <i class='fa fa-heart  faa-pulse animated fa-3x  '></i> <span style="font-size: 48px">LES <span class="text-red">CO</span>MMANDEMENTS</span></span>
+</div>
+<div class="space20"></div>
+
+<div class="keywordList"></div>
+
+<script type="text/javascript">
+var keywords = [
+	
+	
+	{
+		"title":"Don’t threaten people.",
+		"body":"This includes threats of violence, and threats to hurt others in any way."
+	},
+	{
+		"title":"Don’t post others’ personal information",
+		"body":"Don’t post any personal information of any other users or third parties unless you have express permission from them to do so."
+
+	},
+	{
+		"title":"Don’t hate",
+		"body":"Slurs or hate speech, including denigrating comments involving race, sexual orientation, religious or political belief, age, or just about anything else, whether they be personal or general, besides being very uncool, are not allowed."
+
+	},
+	{
+		"title":"Don’t promote self-harm.",
+		"body":"Do not post content that glorifies self-harm. Do not encourage others to harm themselves. Communecter is a safe space to support recovery."
+
+	},
+	{
+		"title":"Don’t spam",
+		"body":"Don’t use fake tags or deceptive links or try to mislead others about the things you post. Don’t use malware or other evil code when you post. Don’t use Communecter as a vehicle for generating revenue for affiliate marketing. No phishing. Don’t follow massive amounts of users for the sole purpose of leading them to your profile. When you follow someone, they are sent an e-mail notification, and thus following too many users is considered spam."
+
+	},
+	{
+		"title":"Don’t automate",
+		"body":"Communecter is for human beings. It is against the rules to use a computer to register a lot of users on Communecter, or to follow lots of users, or to automate posting."
+	},
+	{
+		"title":"Don’t infringe on other people’s copyrights and trademarks",
+		"body":"Don’t post content that belongs to third parties without their consent. Please make sure you have the right to post the content that you put on Communecter. In other words, only post content that you own or have permission to post."
+	},
+	{
+		"title":"Be legal",
+		"body":"Only post content that is legal, meaning you can’t post content that violates a law, rule or regulation."
+	},
+	{
+		"title":"Don’t squat on usernames",
+		"body":"Don’t register a username that you are not going to use yourself. Don’t sell your username. Don’t register a username just to prevent someone else from using it. Usernames that have not been actively used for an extended period will be reclaimed."
+	},
+	{
+		"title":"Don’t impersonate others",
+		"body":"Be yourself, or a version of yourself. Please don’t pretend to be someone else."
+	},
+	{
+		"title":"Don’t hurt young people",
+		"body":"Don’t post anything inappropriate about minors, including things that are violent or sexual in nature."
+	},
+	{
+		"title":"Don’t use Communecter to intrude on other people’s privacy",
+		"body":"There’s enough of that going on by big internet and governments around the world."
+	},
+	{
+		"title":"NSFW",
+		"body":"If you regularly post adult content, you must respect those in our community who do not wish to see it by flagging your profile Not Safe for Work (NSFW).<br/>To mark your account NSFW, go to your settings page under the heading, 'Post Adult-Oriented Content.'<br>If you regularly post adult content and do not mark your account NSFW, we may ask you to do so, or we may flag your account to NSFW for you. If you abuse our NSFW rules, your account may be frozen or deleted.<br/>If you choose not to view adult content, NSFW posts will be hidden behind a link that says “Click to show NSFW Media”, and NSFW user avatars and headers will be pixelated."
+	},
+	{
+		"title":"Don’t be a nuisance regarding technology",
+		"body":"Don’t try to circumvent or compromise our security or access non-public areas of the Communecter website. Don’t use robots or other automated technology to access Communecter. Do not scrape content from Communecter. Don’t try to deceive other users or the public. If someone has blocked you, respect their decision and do not try to communicate with them through an alternative account. Don’t interfere with other users use of Communecter. Don’t intentionally load test our Service or see how much traffic it can handle. Don’t overload our servers with traffic. Don’t upload or distribute viruses or malware to or through Communecter.<br/><br/>Communecter’s rules expressly forbid posting abusive content of any kind, including content of any kind that portrays or encourages violence towards people or animals.<br/><br/>If you break the rules we may contact you and ask you to fix the problem immediately.<br/><br/>If you don’t, your account may be suspended or deleted, your username forfeited, and your IP address may be permanently banned from logging into Communecter. We reserve the right to enforce or not to enforce these rules in whatever way we see fit, at our sole discretion.<br/><br/>According to our lawyer, we should also tell you that Communecter rules and policies do not create a duty or contractual obligation for us to act in any particular manner. And we reserve the right to change these rules at any time. So please play nice, be respectful, and have fun.<br/><br/>If you notice abusive behavior, please report it immediately by emailing reportabuse@ello.co and we will look into it, and take necessary action as soon as possible. This email box is strictly for reporting abuse, and we will not respond to other requests sent here."
+	}
+];
+	
+
+jQuery(document).ready(function() 
+{
+	getConceptList(keywords, ".keywordList", "list");
+	/*$(".keywordList").html('');
+	$.each(keywords,function(i,obj) { 
+		icon = (obj.icon) ? obj.icon : "fa-tag" ;
+		color = (obj.color) ? obj.color : "#E33551" ;
+		$(".keywordList").append(
+		'<div class="panel panel-white">'+
+			'<div class="panel-heading border-light ">'+
+				'<span class="panel-title homestead"> <i class="fa '+icon+'  fa-2x"></i> <span style="font-size: 35px; color:'+color+';"> '+obj.title.toUpperCase()+'</span></span>'+
+			'</div>'+
+			'<div class="panel-body">'+
+				'<blockquote class="space20">'+
+					obj.body+
+				 "</blockquote>"+
+			"</div>"+
+		"</div>");
+	 });*/
+});
+
+</script>
+

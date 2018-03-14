f3ffc77a8f2de80ea59e4d082f7116263a3f85f1
Oceatoon <oceatoon@gmail.com>
2018-03-14 11:37:59 +0400

    explained gami

--- views/element/about.php
+++ views/element/about.php
@@ -329,7 +329,7 @@
 						<i class="fa fa-hashtag"></i> <?php echo Yii::t("common","Gamification"); ?> :
 					</span>
 					<?php 	
-								echo Gamification::calcPoints( Yii::app()->session["userId"] ).Yii::t("common","pts , based on your Links and Posts");
+								echo Gamification::calcPoints( Yii::app()->session["userId"] ).Yii::t("common","pts , based on Links and Posts popularity");
 							 ?>	
 				</div>
 			</div>

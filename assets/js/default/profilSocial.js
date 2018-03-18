6d84bc0185a940ed1841822b2d57b51de2e8f486
Bouboule <clement.damiens@gmail.com>
2018-03-19 00:06:20 +0100

    hotfix no possible to click on menu community directory

--- assets/js/default/profilSocial.js
+++ assets/js/default/profilSocial.js
@@ -876,7 +876,7 @@ function displayInTheContainer(data, dataName, dataIcon, contextType, edit){
 				if(col=="favorites")
 					colName="favoris";
 				html += "<a class='btn btn-default col-xs-12 shadow2 padding-10 margin-bottom-20' onclick='$(\"."+colName+"\").toggleClass(\"hide\")' ><h2><i class='fa fa-star'></i> "+colName+" ("+Object.keys(val.list).length+")</h2></a>"+
-						"<div class='"+colName+" hide'>";
+						"<div class='"+colName+" col-sm-12 col-xs-12  hide'>";
 				mylog.log("list", val);
 				if(val.count==0)
 					html +="<span class='col-xs-12 text-dark margin-bottom-20'>"+trad.noelementinthiscollection+"</span>";
@@ -934,7 +934,7 @@ function displayInTheContainer(data, dataName, dataIcon, contextType, edit){
 		//var nothing = tradException.no;
 		//if(dataName == "organizations" || dataName == "collections" || dataName == "follows")
 		//	nothing = tradException.nofem;
-		var html =  communityStr+"<div class='col-md-12 margin-bottom-15'>"+
+		var html =  communityStr+"<div class='col-md-12 col-sm-12 col-xs-12  margin-bottom-15'>"+
 						getLabelTitleDir(dataName, dataIcon, parseInt(n), n)+
 					"</div>";
 		$("#central-container").html(html + "<span class='col-md-12 alert bold bg-white'>"+

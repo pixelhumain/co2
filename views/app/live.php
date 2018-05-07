0f370fd688f9610723937cc6617f3aa83b5fb0ed
Bouboule <clement.damiens@gmail.com>
2018-05-07 15:38:00 +0200

    remove all search text => automatic reload #861

--- views/app/live.php
+++ views/app/live.php
@@ -230,7 +230,7 @@ jQuery(document).ready(function() {
     $("#main-search-bar").keyup(function(e){
         $("#second-search-bar").val($(this).val());
         $("#input-search-map").val($(this).val());
-        if(e.keyCode == 13){
+        if(e.keyCode == 13 || $(this).val() == ""){
             startNewsSearch(true); 
             KScrollTo("#content-social");
         }
@@ -242,7 +242,7 @@ jQuery(document).ready(function() {
     $("#second-search-bar").keyup(function(e){
         $("#main-search-bar").val($(this).val());
         $("#input-search-map").val($(this).val());
-        if(e.keyCode == 13){            
+        if(e.keyCode == 13 || $(this).val() == ""){            
             startNewsSearch(true);
             KScrollTo("#content-social");
          }

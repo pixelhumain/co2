20741237f37435ad1916f38ca4eaf34b1b24fe1c
Bouboule <clement.damiens@gmail.com>
2018-06-14 15:23:17 +0200

    add es and it file in docs

---
+++ views/docs/es/about.php
@@ -0,0 +1,177 @@
+<div class="panel-heading border-light center text-dark partition-white radius-10">
+    <span class="panel-title homestead"> <i class='fa fa-users faa-pulse animated fa-3x  '></i> <span style="font-size: 48px">Association <span class="text-red">Open Atlas</span></span></span>
+</div>
+
+<div style="text-align: left;padding:15px; font-size: 20px">
+
+	<br><br>
+	L'association Open Atlas est un espace multidisciplinaire d'échanges permettant de développer des projets dans des domaines de l’intérêt général, notamment autour de l’économie de l’information et de la communication.
+	<br>Intérêt social et solidaire, l’association s’intéresse à tous les territoires, du local à
+	l’international et oeuvre, en toute diversité, dans les champs du développement humanitaire, culturel, artistique et de la communication. L’association se donne également pour objectif d’être un laboratoire et une cellule active de recherche et de développement, défendant le décloisonnement et le partage des compétences, ainsi que la liberté de penser et d’agir.
+	<br><br>
+	<a href="http://open-atlas.org" target="_blank">Tout savoir sur Open Atlas</a>
+</div>
+
+
+<div class="space20"></div>
+
+<div class="panel-heading border-light center text-dark partition-white radius-10">
+    <span class="panel-title homestead "> <i class='text-red fa fa-search faa-pulse animated fa-3x  '></i> <span style="font-size: 48px"><span class="text-red">CO</span>.R.D</span> </span>
+</div> 
+<div style="text-align: left;padding:15px;font-size: 20px">
+Le collectif informel C.O.R.D (CO & Open R&D), une future SCIC, regroupe des acteurs du numérique (web, mobile, design, objets connectés). Elle est spécialisée dans les modes de communication innovants et défend depuis toujours l’innovation et la philosophie Open Source comme vecteurs de Collaboration Massive. 
+<br><br>
+CORD regroupe une communauté de talents et d'expertises, bénéficiant de retours d’expérience éprouvés. Son objet est d'accompagner les entreprises et associations dans leur démarche d'innovation. Un clusters dédié à la recherche et au développement sous une forme décentralisé.
+ <br><br>
+Nous offrons une gamme de produits et services à forte valeur ajoutée :
+<br>Développement de solutions numériques
+<br>Création Graphique (logo, direction artistique)
+<br>Développement de Hardware (Arduino, capteurs, imprimante 3D…)
+<br>Barcamp / Atelier collaboratif + créatif (Brainstorming, Serious game, Hackathon…)
+<br>Formations : outils collaboratifs, organisation projet, développement web, design…
+<br>Hébergement (hosting cloud ou dédié)
+<br>Animation Vidéo (design, motion...)
+<br>E-commerce
+<br>Installation et animation digitale (design intéractif)
+<br>Conseil et Think Tank innovation
+<br>Cartographie (SIG : Système d’information géographique)
+ <br><br>
+CORD promeut l’intelligence collective et l’initiative citoyenne en militant pour la conjugaison de l’éthique et de l’efficacité, autour de l’économie numérique et en oeuvrant dans le principe de développement soutenable, durable et solidaire.
+</div>
+
+<div class="space20"></div>
+
+<style type="text/css">
+	.keypane{
+		height: 325px;
+		text-align: left
+	}
+</style>
+
+
+<div class="panel-heading border-light center text-dark partition-white radius-10">
+    <span class="panel-title homestead"> <i class='text-red fa fa-lightbulb-o faa-pulse animated fa-3x  '></i> <span style="font-size: 48px">PROJETS</span></span>
+</div>
+<div class="space20"></div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-cube"></i> PIXEL HUMAIN </h1>
+	<div>
+		Un collectif d'acteurs oeuvrant pour les communs
+		<br>l'ancien nom de communecter
+		<br>Innovation au service des biens communs
+		<br><u><a class="text-white" href="#" onclick="toastr.error('TODO : ajax form load form')"><i class="fa fa-mail"></i> Contact Us </a> </u>
+		<br>Respectant un CODE SOCIAL ET LOGICIEL ouvert
+		<br>Open Source, Semantique et Interopérable
+		<br>Le Lien entre le réél et le virtuel
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/CO2r.png" height=50> Communecter </h1>
+	<div>
+		Un Réseau Sociétale Libre et Open Source 
+		<br>un dispositif de réseau sociétal local catalyseur d'une synergie régionale, collective et solidaire reliant tous types d’acteurs (Entreprises, Associations, Collectivités et citoyens), tous domaines et objectifs confondus.
+		<br>La plateforme est un service d'aide à la population pour améliorer la valorisation des acteurs locaux pour produire un térritoire connecté.
+		<br>Un outil de communication entre citoyens et collectivités.
+		<br>Un outil de production et de visualisation d'open data pour mieux analyser et décider sur un térritoire.
+		<br>Une boite outil citoyenne pour encourager , faciliter et dynamiser l'implication citoyenne.
+		<br>le CTK : Citizen Tool Kit , Un socle technique et modulaire pour construire toutes sorte d'outils citoyens et administratifs spécialisés
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-map-marker"></i> Network <br>Marque Blanche  </h1>
+	<div>
+		Une Interface et Cartographie 
+		<br>Configurable 
+		<br>Filtrable 
+		<br>R&D : sans base de donnée
+		<br>Contributive
+		<br><a class="btn btn-default btn-sm" href="https://www.communecter.org/?network=BretagneTelecom" target="_blank">Exemple : Bretagne Telecom</a> 
+		<br><a class="btn btn-default btn-sm" href="https://chat.lescommuns.org/channel/marqueblanche" target="_blank">En savoir plus </a>
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-wifi"></i> Smarterre</h1>
+	<div>
+		Apporter de la croissance aux organisations locales avec une vision humaine, intégrer un projet de société locale pour les générations futures qui prend en compte l’obsolescence et la finitude des ressources
+		<br><a class="btn btn-default btn-sm" href="https://github.com/pixelhumain/smarterre" target="_blank">En savoir plus </a>
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-cogs"></i> FABLAB.re</h1>
+	<div>
+		Fablab de bidouilleurs réunionais qui auront bientot un bus et un tiers lieux
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-connectdevelop"></i> Open Système </h1>
+	<div>
+		Expérimentation de gouvernance totalement horizontale 
+		<br>Opensource 
+		<br>Conseil Collégiale
+		<br><a class="btn btn-default btn-sm" href="https://chat.lescommuns.org/channel/marqueblanche" target="_blank">En savoir plus </a>
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-bus"></i> CO BUS </h1>
+	<div>
+		Un bus itinérant Communecté 
+		<br>sponsorisé par l'AFNIC
+		<br>rempli d'electronique 
+		<br>autonome energiquement 
+		<br>pour agir dans les zones prioritaires et déconnecté
+		<br><a class="btn btn-default btn-sm" href="https://chat.lescommuns.org/channel/co_bus" target="_blank">En savoir plus </a>
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-wifi"></i> CO PI </h1>
+	<div>
+		CO : Communecter + PI : Raperi Pi
+		<br>Un serveur Low Tech Communecter
+		<br>fonctionne offline
+		<br>sur un modèle distribué, interconnection entre COPI 
+		<br>servant tout les service de CO
+		<br><a class="btn btn-default btn-sm" href="https://chat.lescommuns.org/channel/copi" target="_blank">En savoir plus </a>
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-cubes"></i> Tiers Lieux <br> Camp TIC</h1>
+	<div>
+		Equipement autonome et connecté pour partager l'activité d'un tiers lieux
+		<br>sponsorisé par l'AFNIC
+		<br>C'est un valise complète 
+		<br>Connectique réseau internet 
+		<br>Son et Video pour des visios partages
+		<br>des Agenda partagé entre réseau de tiers lieux 
+		<br>partage de ressources, de besoins, de service et de compétence 
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-wifi"></i> FAI Libre Reunion </h1>
+	<div>
+		Installation d'un FAI libre Réunionais 
+		<br>sponsorisé par l'AFNIC
+	</div>
+</div>
+
+<div class="col-xs-4 keypane">
+	<h1><i class="text-red fa fa-book"></i> COopedia</h1>
+	<div>
+		R&D : Augmenter Wikipedia avec un référencement et une inteorpérabilité avec CO
+		<br> Terrapedia : wikipedia territorial ouvert et contributif 
+	</div>
+</div>
+<div class="space20"></div>
+
+<script type="text/javascript">
+
+</script>
---
+++ views/docs/es/openatlas.php
@@ -0,0 +1,60 @@
+<div class="panel-heading border-light center text-dark partition-white radius-10">
+    <span class="panel-title homestead"> <i class='fa fa-users faa-pulse animated fa-3x  '></i> <span style="font-size: 48px">Association <span class="text-red">Open Atlas</span></span></span>
+</div>
+
+<div style="text-align: left;padding:15px; font-size: 20px">
+
+	<br><br>
+	L'association Open Atlas est un espace multidisciplinaire d'échanges permettant de développer des projets dans des domaines de l’intérêt général, notamment autour de l’économie de l’information et de la communication.
+	<br>Intérêt social et solidaire, l’association s’intéresse à tous les territoires, du local à
+	l’international et oeuvre, en toute diversité, dans les champs du développement humanitaire, culturel, artistique et de la communication. L’association se donne également pour objectif d’être un laboratoire et une cellule active de recherche et de développement, défendant le décloisonnement et le partage des compétences, ainsi que la liberté de penser et d’agir.
+	<br><br>
+	L'association promeut l’action, l’intelligence collective et l’initiative citoyenne. Elle milite pour
+	la conjugaison de l’éthique et de l’efficacité, autour de l’économie numérique dans le
+	principe du développement soutenable, durable et solidaire. 
+	<br><br>
+	L’association agit sous la gouvernance d’un Conseil d’Administration composé de :
+	<br>Luc Lavielle (Président) Gilles Guillon (Secrétaire) Pierre Magnin (Trésorière) Fabien Matha , Julien Matha 
+	<br>en 2017, s'organisera une restructuration de la gouvernance en conseil collégial.
+	<br><br>
+	Au quotidien, Open Atlas fonctionne grâce à la présence permanente des développeurs, des commoners, et des acteurs sur le terrain, le meilleur moyen de les contacter est <a href="chat.lescommuns.org" target="_blank">ici</a>  .
+	<br>Numéro siret : 513 381 830 00019
+</div>
+
+
+<div class="space20"></div>
+
+<div class="panel-heading border-light center text-dark partition-white radius-10">
+    <span class="panel-title homestead "> <i class='text-red fa fa-search faa-pulse animated fa-3x  '></i> <span style="font-size: 48px"><span class="text-red">CO</span>.R.D</span> </span>
+</div> 
+<div style="text-align: left;padding:15px;font-size: 20px">
+Le collectif informel C.O.R.D (CO & Open R&D), une future SCIC, regroupe des acteurs du numérique (web, mobile, design, objets connectés). Elle est spécialisée dans les modes de communication innovants et défend depuis toujours l’innovation et la philosophie Open Source comme vecteurs de Collaboration Massive. 
+<br><br>
+CORD regroupe une communauté de talents et d'expertises, bénéficiant de retours d’expérience éprouvés. Son objet est d'accompagner les entreprises et associations dans leur démarche d'innovation. Un clusters dédié à la recherche et au développement sous une forme décentralisé.
+ <br><br>
+Nous offrons une gamme de produits et services à forte valeur ajoutée :
+<br>Développement de solutions numériques
+<br>Création Graphique (logo, direction artistique)
+<br>Développement de Hardware (Arduino, capteurs, imprimante 3D…)
+<br>Barcamp / Atelier collaboratif + créatif (Brainstorming, Serious game, Hackathon…)
+<br>Formations : outils collaboratifs, organisation projet, développement web, design…
+<br>Hébergement (hosting cloud ou dédié)
+<br>Animation Vidéo (design, motion...)
+<br>E-commerce
+<br>Installation et animation digitale (design intéractif)
+<br>Conseil et Think Tank innovation
+<br>Cartographie (SIG : Système d’information géographique)
+ <br><br>
+CORD promeut l’intelligence collective et l’initiative citoyenne en militant pour la conjugaison de l’éthique et de l’efficacité, autour de l’économie numérique et en oeuvrant dans le principe de développement soutenable, durable et solidaire.
+</div>
+
+<div class="space20"></div>
+
+<style type="text/css">
+	.keypane{
+		height: 325px;
+		text-align: left
+	}
+</style>
+
+

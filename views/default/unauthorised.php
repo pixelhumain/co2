005e1b1d01c941e792fc6a46bb80be72107ef79a
Oceatoon <oceatoon@gmail.com>
2018-07-02 09:12:23 +0400

    generic unautherised page and secureLogin

---
+++ views/default/unauthorised.php
@@ -0,0 +1,4 @@
+<div class="col-lg-offset-1 col-lg-10 col-xs-12 margin-top-50 text-center text-red">	
+		<i class='fa fa-lock fa-4x '></i><br/>
+		<h1 class=''><?php echo Yii::t("project", "Unauthorized Access.")?></h1>
+</div>

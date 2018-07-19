dbc94b2ae67e2239949fd490a9cc18649695c2df
codev <raphaelriviere06@gmail.com>
2018-07-19 09:50:18 +0400

    Modif

--- views/custom/test.php
+++ views/custom/test.php
@@ -1,13 +1,46 @@
+<style>
+#ulhva {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+#ulhva li {
+    float: left;
+}
+
+#ulhva li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+#ulhva li a:hover:not(.active) {
+    background-color: #f5863f;
+}
+
+#ulhva .active {
+    background-color: #de2147;
+}
+</style>
+
 <div class="col-xs-12" >
 	<div class="col-xs-12" >
 		<img src="<?php echo Yii::app()->getModule("co2")->assetsUrl."/images/custom/hva/banniere.jpg" ;?>" class="img-responsive" style="display: block; margin: 0 auto;">
 	</div>
 	<div class="container col-xs-12" >
-		<ul>
-			<li><a id="btnOrga" href="javascript:;" class="">Orga</a></li>
-			<li><a id="btnEvent" href="javascript:;" class="">Event</a></li>
-		</ul>
-		<iframe id="networkIframe" src="http://127.0.0.1/ph/network/default/index/?src=HVAOrga" class="col-md-10 col-md-offset-2 col-xs-12" style="height:650px;"></iframe>
+		<center><ul id="ulhva">
+			
+				<li class="active"><a id="btnOrga" href="javascript:;" class="">Acteurs</a></li>
+				<li><a id="btnEvent" href="javascript:;" class="">Evénements</a></li>
+			
+			
+		</ul></center>
+		<iframe id="networkIframe" src="http://127.0.0.1/ph/network/default/index/?src=HVAorga" class="col-md-10 col-md-offset-1 col-xs-12" style="height:650px;"></iframe>
 	</div>
 
 </div>
@@ -18,12 +51,12 @@
 	jQuery(document).ready(function() {
         $("#btnOrga").click(function(){
         	alert("btnOrga");
-             $("#networkIframe").attr("src", "http://127.0.0.1/ph/network/default/index/?src=HVAOrga");
+             $("#networkIframe").attr("src", "http://127.0.0.1/ph/network/default/index/?src=HVAorga");
         });
 
         $("#btnEvent").click(function(){
         	alert("btnEvent");
-             $("#networkIframe").attr("src", "http://127.0.0.1/ph/network/default/index/?src=HVAEvent");
+             $("#networkIframe").attr("src", "http://127.0.0.1/ph/network/default/index/?src=HVAevent");
         });
 
     });
